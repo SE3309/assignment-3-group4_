8ac402752b08f76bad9ad5c86665edc88b579f10
Tomasz Wojtas <twojtas@uwo.ca>
2025-11-17 13:49:10 -0500

    Fixing typo errors and checking if queries use existing values

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -1,13 +1,13 @@
 /* Finding all managers that work in London warehouses using
 JOIN */
 SELECT
-  e.employeeID, e.firstName, e.lastName, e.email, e.branch, e.warehouseID
+  e.employeeID, e.firstName, e.lastName, e.email, e.warehouseID, e.empRole
 FROM Employee AS e
 JOIN Warehouse AS w
   ON e.warehouseID = w.warehouseID
 JOIN City AS c
   ON w.postalCode = c.postalCode
-WHERE e.role = 'Manager'
+WHERE e.empRole = 'Manager'
   AND c.city = 'London'
 ORDER BY e.lastName, e.firstName;
 
@@ -15,28 +15,28 @@ ORDER BY e.lastName, e.firstName;
 
 /* Showing all rooms that cost above the average price for 
 their own storage type using AVG */
-SELECT r.roomID, r.roomNo, r.storageType, r.rentalPrice
-FROM Room AS r
+SELECT r.roomID, r.roomNo, r.storageType, r.rentalPricePerDay
+FROM StorageRoom AS r
 WHERE r.rentalPrice >
-      (SELECT AVG(r2.rentalPrice)
-       FROM Room AS r2
+      (SELECT AVG(r2.rentalPricePerDay)
+       FROM StorageRoom AS r2
        WHERE r2.storageType = r.storageType)
-ORDER BY r.storageType, r.rentalPrice DESC;
+ORDER BY r.storageType, r.rentalPricePerDay DESC;
 
 
 
 /* Shows all warehouses that have at least one available 
 cold storage room using EXISTS semi-join */
 SELECT
-  w.warehouseID, w.street, c.city
+  w.warehouseID, w.street, p.postalcode
 FROM Warehouse AS w
-JOIN City AS c
-  ON w.postalCode = c.postalCode
+JOIN PostalArea AS p
+  ON w.postalCode = p.postalCode
 WHERE EXISTS (
   SELECT 1
-  FROM Room AS r
+  FROM StorageRoom AS r
   WHERE r.warehouseID = w.warehouseID
     AND r.storageType = 'Cold Storage'
     AND r.availabilityStatus = 'Available'
 )
-ORDER BY c.city, w.warehouseID;
+ORDER BY p.postalCode, w.warehouseID;
