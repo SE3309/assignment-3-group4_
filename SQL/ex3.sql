ca43d09ae87f4d928b2ba31cc70ada750c536b05
Shehbaaz Virk <svirk27@uwo.ca>
2025-11-17 14:21:03 -0500

    Updated insert statements

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -5,7 +5,7 @@ phoneNo = '6471234567',
 dateOfBirth = '1998-05-12' , 
 email = 'amrit.singh@gmail.com', 
 street= '12B King Street',
-postalCode = 'M5V 2T6';
+postalCode = 'A2J 7E2';
 
 INSERT INTO Customer (firstName, lastName, phoneNo, dateOfBirth, email, street, postalCode)
 SELECT
@@ -16,6 +16,6 @@ WHERE email = 'amrit.singh@gmail.com';
 INSERT INTO Customer 
 (firstName, lastName, phoneNo, dateOfBirth, email, street, postalCode)
 VALUES
-('Simran', 'Dhaliwal', '4375557890', '1999-08-30', 'simran.dhaliwal@gmail.com', '22A Front Street', 'L6R 2J3'),
-('Raj', 'Patel', '9056671234', '1987-03-18', 'raj.patel@gmail.com', '45 Bay Street', 'N6R 5H3');
+('Simran', 'Dhaliwal', '4375557890', '1999-08-30', 'simran.dhaliwal@gmail.com', '22A Front Street', 'X7C 5X8'),
+('Raj', 'Patel', '9056671234', '1987-03-18', 'raj.patel@gmail.com', '45 Bay Street', 'T4H 9R8');
 
