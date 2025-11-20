10bcc18b55de18f381be4c9389b14c6539407f08
Parth Valand <pvaland@uwo.ca>
2025-11-20 12:07:25 -0500

    Created the 3 data modifications commands

---
+++ SQL/ex6.sql
@@ -0,0 +1,34 @@
+UPDATE StorageRoom
+SET availabilityStatus = 'Available'
+WHERE leaseID IN (
+    SELECT leaseID
+    FROM LeaseAgreement
+    WHERE DATE(endDate) < DATE(NOW())
+);
+
+
+
+-- -----------------------------------
+
+DELETE FROM Transactions t
+WHERE t.transactionStatus = 'Cancelled'
+  AND t.amount < 200
+  AND t.transactionDate < DATE_SUB(CURDATE(), INTERVAL 18 MONTH)
+  AND EXISTS (
+        SELECT 1
+        FROM Customer c
+        WHERE c.email = t.email
+          -- AND c.city = 'Toronto'
+          AND c.dateOfBirth < DATE('1990-01-01')
+  );
+
+
+-- ----------------------------------
+
+UPDATE StorageRoom
+SET rentalPricePerDay = rentalPricePerDay * 1.15
+WHERE storageType = 'Bulk Storage';
+
+
+
+ 
